d288e741f56b8519403c8e87f9e5c7f955b7d943
mingyuanZang <mingyuanzang@gmail.com>
2025-06-04 08:44:02 +0000

    Fix a bug

--- use_case_cic/FL_controller.py
+++ use_case_cic/FL_controller.py
@@ -1,6 +1,6 @@
 ########################################################################
 # Copyright (c) Mingyuan Zang, Tomasz Koziak, Networks Technology and Service Platforms Group, Technical University of Denmark
-# This work was partly done at Computing Infrastructure Group, University of Oxford
+# This work was partly done with Changgang Zheng at Computing Infrastructure Group, University of Oxford
 # All rights reserved.
 # E-mail: mingyuanzang@outlook.com
 # Licensed under the Apache License, Version 2.0 (the 'License')
